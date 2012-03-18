f37716a408467d7f12cea58430f54dd222ed6545
Chenxiong Qi <qcxhome@gmail.com>
2012-03-18 17:17:50 +0800

    add more information to package

--- setup.py
+++ setup.py
@@ -8,12 +8,28 @@ def get_long_description():
 setup(
     name = 'krbcontext',
     version = '0.1',
+    description = 'A Kerberos context manager',
     long_description = get_long_description(),
-    keywords = 'kerberos context principal credential',
-    license = 'BSD',
+    keywords = ['krb', 'kerberos', 'context', 'principal', 'credential'],
+    license = '',
     author = 'Chenxiong Qi',
     author_email = 'qcxhome@gmail.com',
 
     packages = find_packages('src'),
     package_dir = { '': 'src', },
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration',
+    ],
 )
