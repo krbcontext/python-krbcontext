5eda62aea0d1d8bd789bf793d50111b89046cea5
Chenxiong Qi <qcxhome@gmail.com>
2017-08-27 14:19:10 +0800

    Add script for publishing packages
    Signed-off-by: Chenxiong Qi <qcxhome@gmail.com>

---
+++ scripts/publish-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(dirname $(realpath $0))/../"
+
+RELEASE_DIR="$(realpath release)"
+
+name=$(python -c "import ConfigParser; cfg=ConfigParser.RawConfigParser(); cfg.read('setup.cfg'); print(cfg.get('package', 'name'))")
+rel_ver=$(python -c "import ConfigParser; cfg=ConfigParser.RawConfigParser(); cfg.read('setup.cfg'); print(cfg.get('package', 'version'))")
+
+twine upload -u tkdchen "${RELEASE_DIR}/${name}-${rel_ver}.tar.gz"
+
+srpm_nvr=$(rpm -q --qf "%{NVR}\n" --specfile ${specfile} | head -n 1)
+copr-cli --config ~/.config/copr-fedora build cqi/python-krbcontext "${RELEASE_DIR}/${srpm_nvr}.src.rpm"
